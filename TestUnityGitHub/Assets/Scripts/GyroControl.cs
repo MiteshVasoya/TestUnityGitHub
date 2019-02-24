c1b1bf0595b8f801d449c529d06f298cc886621c
Mitesh Vasoya <miteshvasoya@Miteshs-MacBook-Pro.local>
2019-02-24 17:15:32 -0500

    Test Scripts added
    Change view based on devices aspect ratio script added
    Gyro control script added

---
+++ TestUnityGitHub/Assets/Scripts/GyroControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour
+{
+
+    private bool gyroEnabled;
+    private Gyroscope gyroscope;
+
+    private GameObject cameraContainer;
+    private Quaternion rot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if(SystemInfo.supportsGyroscope)
+        {
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rot = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gyroEnabled)
+        {
+            transform.localRotation = gyroscope.attitude * rot;
+        }
+    }
+}
