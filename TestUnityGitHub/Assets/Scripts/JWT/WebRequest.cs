5cdd97c91fcaf179da6684a5b7c7d512ddf2fe78
Mitesh Vasoya <vasoya.2@wright.edu>
2019-04-24 17:05:03 -0400

    JWT Scene added

---
+++ TestUnityGitHub/Assets/Scripts/JWT/WebRequest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebRequest : MonoBehaviour
+{
+    private string ENDPOINT = "https://jsonplaceholder.typicode.com/posts";
+    public Text responseText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Request();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Request()
+    {
+        WWWForm form = new WWWForm();
+
+        WWW request = new WWW(ENDPOINT);
+        StartCoroutine(OnResponse(request));
+    }
+
+    private IEnumerator OnResponse(WWW req)
+    {
+        yield return req;
+
+        responseText.text = req.text;
+    }
+}
