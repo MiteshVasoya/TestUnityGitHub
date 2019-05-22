1137c5ff0ea4a7d1a541472f72538e56af1efcfe
Mitesh Vasoya <vasoya.2@wright.edu>
2019-05-22 17:47:45 -0400

    Minimize scene added
    Test if application can be closed from minimized state.

---
+++ TestUnityGitHub/Assets/Scripts/MinimizedScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimizedScript : MonoBehaviour
+{
+    int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+    
+    private void OnApplicationFocus(bool focus)
+    {
+        count++;
+        Debug.Log("OnApplicationFocus: " + focus);
+        if (!focus && count == 2)
+        {
+            Application.Quit();
+        }
+    }
+}
